32f161bc83a1be0f6c6ee72df6b5da428e8d65f5
DEFLY <eloifabius@gmail.com>
2020-05-23 17:34:21 +0200

    initialisation

--- src/App.js
+++ src/App.js
@@ -6,18 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <h1>K O S S I D E F L Y</h1>
+          <h3 style={{margin: 0, color: "grey"}}>under construction ;)</h3>
       </header>
     </div>
   );
